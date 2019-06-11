03c38fd827cf590d872e25be56b2f77897faf448
Fernando Alejandro Yerena Ramos <ferdinandalexandros@gmail.com>
2019-06-11 13:48:38 -0500

    Obtaining input values

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const $DESC = document.getElementById('description');
+const $CALORIES = document.getElementById('calories');
+const $CARBS = document.getElementById('carbs');
+const $PROTEIN = document.getElementById('protein');
+

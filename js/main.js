07e5c2302c9e6a32f5d35438618d23c6d1a2e86e
Fernando Alejandro Yerena Ramos <ferdinandalexandros@gmail.com>
2019-06-11 16:57:05 -0500

    Adding input validation and error styles

--- js/main.js
+++ js/main.js
@@ -2,4 +2,39 @@ const $DESC = document.getElementById('description');
 const $CALORIES = document.getElementById('calories');
 const $CARBS = document.getElementById('carbs');
 const $PROTEIN = document.getElementById('protein');
+const $ADD_BUTTON = document.getElementById('add');
 
+const validateInputs = () =>
+{
+
+    if (
+        $DESC.value &&
+        $CALORIES.value &&
+        $CARBS.value &&
+        $PROTEIN.value
+    )
+    {
+        console.log('OK');
+    }
+    else
+    {
+        $DESC.classList.add('is-invalid');
+        $CALORIES.classList.add('is-invalid');
+        $CARBS.classList.add('is-invalid');
+        $PROTEIN.classList.add('is-invalid');
+    }
+}
+
+$ADD_BUTTON.addEventListener('click', validateInputs);
+$DESC.addEventListener('keyup', ()=>{
+    $DESC.classList.remove('is-invalid');
+});
+$CALORIES.addEventListener('keyup', ()=>{
+    $CALORIES.classList.remove('is-invalid');
+});
+$CARBS.addEventListener('keyup', ()=>{
+    $CARBS.classList.remove('is-invalid');
+});
+$PROTEIN.addEventListener('keyup', ()=>{
+    $PROTEIN.classList.remove('is-invalid');
+});

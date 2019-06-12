fe3bfb7280aaf75a7919ef4d946a6ce462b34527
Fernando Alejandro Yerena Ramos <ferdinandalexandros@gmail.com>
2019-06-11 21:56:53 -0500

    Generating HTML elements with JS

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+const compose = (...functions) => data =>
+    functions.reduceRight((value, func) => func(value), data)
+
+/*
+    function compose(...functions) {
+        return function (data) {
+            functions.reduceRight((value, function) => func(value), data){
+            })
+        }
+    }
+*/
+
 const $DESC = document.getElementById('description');
 const $CALORIES = document.getElementById('calories');
 const $CARBS = document.getElementById('carbs');
@@ -7,6 +19,46 @@ const $ADD_BUTTON = document.getElementById('add');
 const IS_INVALID = 'is-invalid'
 
 let itemsList = [];
+
+
+const attrToString = (obj = {}) =>
+{
+    const keys = Object.keys(obj);
+    const attr = [];
+
+    keys.map(() =>
+    {
+        attr.push(`${keys}=${obj[keys]}`)
+    })
+
+    const attrString = attr.join();
+
+    return attrString;
+}
+
+// console.log(attrToString({'class': 'title'}));
+const tagAttrs = obj => (content = '') =>
+    `<${obj.tag}${obj.attrs ? ' ' : ''}${attrToString(obj.attrs)}> ${content} </${obj.tag}>`;
+
+// console.log(tagAttrs({tag: 'tr'})( ' Hola, mundo '));
+
+const tag = nameTag => 
+{
+    if (typeof nameTag === 'string')
+    {
+        return tagAttrs({ tag: nameTag });
+    }
+    else
+    {
+        return tagAttrs(nameTag);
+    }
+};
+
+const tableRowTag = tag('tr');
+const tableRow = (items) => compose(tableRowTag, tableCells)(items);
+
+const tableCell = tag('td');
+const tableCells = items => items.map(tableCell).join('');
 
 const add = (list) =>
 {

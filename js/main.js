f046646933d17ce956246069f8d36335cd6c9a74
Fernando Alejandro Yerena Ramos <ferdinandalexandros@gmail.com>
2019-06-11 19:10:30 -0500

    Adding items to a list when we press button

--- js/main.js
+++ js/main.js
@@ -4,37 +4,54 @@ const $CARBS = document.getElementById('carbs');
 const $PROTEIN = document.getElementById('protein');
 const $ADD_BUTTON = document.getElementById('add');
 
+const IS_INVALID = 'is-invalid'
+
+let itemsList = [];
+
+const add = (list) =>
+{
+    const newItem = {
+        description: $DESC.value,
+        calories: parseInt($CALORIES.value),
+        carbs: parseInt($CARBS.value),
+        protein: parseInt($PROTEIN.value),
+    }
+
+    list.push(newItem);
+    console.log(list);
+}
+
+const clearInputs = () =>
+{
+    $DESC.value = '';
+    $CALORIES.value = '';
+    $CARBS.value = '';
+    $PROTEIN.value = '';
+}
+
 const validateInputs = () =>
 {
 
-    if (
-        $DESC.value &&
-        $CALORIES.value &&
-        $CARBS.value &&
-        $PROTEIN.value
-    )
+    if ($DESC.value && $CALORIES.value && $CARBS.value && $PROTEIN.value)
     {
-        console.log('OK');
+        add(itemsList);
+        clearInputs();
     }
     else
     {
-        $DESC.classList.add('is-invalid');
-        $CALORIES.classList.add('is-invalid');
-        $CARBS.classList.add('is-invalid');
-        $PROTEIN.classList.add('is-invalid');
+        $DESC.classList.add(IS_INVALID);
+        $CALORIES.classList.add(IS_INVALID);
+        $CARBS.classList.add(IS_INVALID);
+        $PROTEIN.classList.add(IS_INVALID);
     }
 }
 
 $ADD_BUTTON.addEventListener('click', validateInputs);
-$DESC.addEventListener('keyup', ()=>{
-    $DESC.classList.remove('is-invalid');
-});
-$CALORIES.addEventListener('keyup', ()=>{
-    $CALORIES.classList.remove('is-invalid');
-});
-$CARBS.addEventListener('keyup', ()=>{
-    $CARBS.classList.remove('is-invalid');
-});
-$PROTEIN.addEventListener('keyup', ()=>{
-    $PROTEIN.classList.remove('is-invalid');
-});
+
+$DESC.addEventListener('keyup', () => { $DESC.classList.remove(IS_INVALID) });
+
+$CALORIES.addEventListener('keyup', () => { $CALORIES.classList.remove(IS_INVALID) });
+
+$CARBS.addEventListener('keyup', () => { $CARBS.classList.remove(IS_INVALID) });
+
+$PROTEIN.addEventListener('keyup', () => { $PROTEIN.classList.remove(IS_INVALID) });
